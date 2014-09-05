134ef4de127f6d5d2e31d6444bf25b612174a3c8
renandeandradevaz <renanandrade_rj@hotmail.com>
2014-09-05 15:14:48 -0300

    adfasf

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,1 +1,1 @@
-MyApp::Application.config.secret_token = ENV['SECRET_TOKEN']
+TestadorDeLinks::Application.config.secret_token = 'fb93371ca669f37c01dbc624538d725ada76a99bde9994a3ec961ef70066e9dbf84e69c720c90a08cbc02255982bda5ccf60571449a9057e5155bf7023895209'
